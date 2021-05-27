c6f4b0f13e6aeecd28fba31f5dd062b3e6541e91
Rushikesh8421 <patilrushikesh8421@gmail.com>
2021-05-27 15:46:22 +0530

    A small programme which calculates the percentage of of marks provided

---
+++ Percentage calculator/src/Percent.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Percent {
+
+	public static void main(String[] args) {
+		float sumOfMarks = 0;
+		float result;
+		float [] marks = new float[10];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the outof marks for each subject:\n");
+		int outOfMarks = sc.nextInt();
+		System.out.println("Enter the no of subjects:\n");
+		int noOfSub = sc.nextInt();
+		int totalMarks = (outOfMarks)*(noOfSub);
+		for(int i = 0;i<noOfSub;i++) {
+			System.out.println("Enter the marks for subject "+(i+1)+" outof "+outOfMarks);
+			marks[i] = sc.nextFloat();
+			sumOfMarks = sumOfMarks+marks[i];
+		}
+		result = (sumOfMarks*100)/totalMarks;
+		System.out.println("Your Percentage is: "+result+"%");
+		
+		
+		
+
+	}
+
+}
